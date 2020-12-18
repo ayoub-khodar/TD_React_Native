fec754a6c0760c825c9f94b8d8d4df557462eecf
Ayoubzoldick <ayoubkhodar1998@gmail.com>
2020-12-18 21:07:44 +0100

    Semaine 18/12/2019

---
+++ sourceFolder1/sourceFolder2/data.js
@@ -0,0 +1,13 @@
+ const data=[{key:'1',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'2',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'3',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'4',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'5',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'}];
+
+export const data1=[{key:'1',firstName:'ahmed', lastName:'Bakkali', class:'GI'},
+{key:'2',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'3',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'4',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'},
+{key:'5',firstName:'Ayoub', lastName:'KHODAR', class:'SIG3'}];
+
+export default data;
